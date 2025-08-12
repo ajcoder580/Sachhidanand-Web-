f4a91a9447cb56bc819ce28c18fefa2c6817f159
Jay Kumar Sharma <157815432+jaykumar108@users.noreply.github.com>
2025-08-12 22:11:00 +0530

    updated db connection

--- models/Db.js
+++ models/Db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongo_uri = process.env.MONGO_URI;
+const mongo_uri = process.env.MONGO_URI ||`mongodb://localhost:27017/sachhidanand-web`;
 
 mongoose.connect(mongo_uri)
   .then(() => {
